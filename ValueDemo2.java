275426778427ed518dadd1842564bbeca1282720
ketyps <171883081+ketyps@users.noreply.github.com>
2026-06-17 00:01:32 +0800

    完善常量制表符演示ValueDemo2

--- ValueDemo2.java
+++ ValueDemo2.java
@@ -1,10 +1,11 @@
 public class ValueDemo2 {
     public static void main(String[] args) {
         //目标:熟悉制表符的基本用法
+        //和C的字符对齐比较像,在写C的99乘法表的时候遇到过
 
-        System.err.println("name" + '\t' + "age");
-        System.err.println("tom" + '\t' +"23");
-        
+        System.out.println("name" + '\t' + "age");
+        System.out.println("tom" + '\t' + "23");
+
     }
     
 }
