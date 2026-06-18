524d3cc93f3973306ef7ad2b1aab0569f7348180
ketyps <guanlin1600@gmail.com>
2026-06-18 22:54:28 +0800

    initialize

---
+++ VariableDemo3.java
@@ -0,0 +1,37 @@
+public class VariableDemo3 {
+    public static void main(String[] args) {
+        //byte
+        byte b=10;
+        System.out.println(b);//10
+        //short
+        short s=20;
+        System.out.println(s);//20
+        //int
+        int i=30;
+        System.out.println(i);//30
+        //long
+        //如果要定义long类型的变量
+        //在数据值后面需要加一个L作为后缀
+        //L是可以大写的,也可以是小写的.
+        //建议:使用大写.
+        long n=9999999999L;
+        System.out.println(n);//9999999999
+
+
+        //flot
+        float f=10.1F;
+        System.out.println(f);//10.1
+        //double
+        double d=20.2;
+        System.out.println(d);//20.2
+
+        //char
+        char c='中';
+        System.out.println(c);//中
+
+        //boolean
+        //true false
+        boolean o=true;
+        System.out.println(o);//true
+    }
+}
