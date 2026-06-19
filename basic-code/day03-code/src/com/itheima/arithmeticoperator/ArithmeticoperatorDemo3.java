36b7b0c4f4c77570bdae6a87ba8d47a0d9eed0ac
ketyps <guanlin1600@gmail.com>
2026-06-19 18:20:37 +0800

    initialize

---
+++ basic-code/day03-code/src/com/itheima/arithmeticoperator/ArithmeticoperatorDemo3.java
@@ -0,0 +1,12 @@
+package com.itheima.arithmeticoperator;
+
+public class ArithmeticoperatorDemo3 {
+    static void main(String[] args) {
+        byte b1=100;
+        byte b2=200;
+        //现在我们要强转的结果是谁?
+        //(byte)b1 + b2 强转的是b1,并不是最终的结果
+        byte result=(byte)(b1+b2);
+        System.out.println(result);//结果就发生错误了,因为要转换的数据过大.
+    }
+}
