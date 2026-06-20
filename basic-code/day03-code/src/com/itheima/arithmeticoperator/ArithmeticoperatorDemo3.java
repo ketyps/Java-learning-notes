9eec0cb12931a9b1945ed17091d4d0f46cb13845
ketyp <guanlin1600@gmail.com>
2026-06-20 14:27:43 +0800

    java字符串连接

--- basic-code/day03-code/src/com/itheima/arithmeticoperator/ArithmeticoperatorDemo3.java
+++ basic-code/day03-code/src/com/itheima/arithmeticoperator/ArithmeticoperatorDemo3.java
@@ -2,8 +2,8 @@ package com.itheima.arithmeticoperator;
 
 public class ArithmeticoperatorDemo3 {
     static void main(String[] args) {
-        byte b1=100;
-        byte b2=200;
+        byte b1=10;
+        byte b2=20;
         //现在我们要强转的结果是谁?
         //(byte)b1 + b2 强转的是b1,并不是最终的结果
         byte result=(byte)(b1+b2);
