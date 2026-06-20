0ca4a7a46b8dec2d866c9fc18238d68477844048
ketyps <guanlin1600@gmail.com>
2026-06-20 16:22:07 +0800

    initialize

--- basic-code/day03-code/src/com/itheima/logicoperator/LogicoperatorDemo1.java
+++ basic-code/day03-code/src/com/itheima/logicoperator/LogicoperatorDemo1.java
@@ -15,6 +15,16 @@ public class LogicoperatorDemo1 {
         System.out.println(false | false);//false
         System.out.println(true | false);//true
         System.out.println(false | true);//true
+
+        //3. 短路逻辑运算符具有短路效果
+        //简单理解：当左边的表达式能确定最终的结果，那么右边就不会参与运行了
+        int a = 10;
+        int b = 10;
+        boolean result = ++a < 5 && ++b < 5;
+        System.out.println(result);//false
+        System.out.println(a);//11
+        System.out.println(b);//11
     }
 }
+//一般或和一般与在C和Python都没有,值得关注一下
 //逻辑运算符,很难不让我想起大一上学期学的离散数学
