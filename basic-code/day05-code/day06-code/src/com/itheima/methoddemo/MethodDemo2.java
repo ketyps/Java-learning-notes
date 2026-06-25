526aa363b494d5228550288548c7dac142177023
ketyps <guanlin1600@gmail.com>
2026-06-25 16:56:29 +0800

    initialize

---
+++ basic-code/day05-code/day06-code/src/com/itheima/methoddemo/MethodDemo2.java
@@ -0,0 +1,16 @@
+package com.itheima.methoddemo;
+
+public class MethodDemo2 {
+    public static void main(String[] args) {
+        // 目标：利用方法最简单的格式完成当前练习
+
+        // 调用方法
+        printlnInfo();
+    }
+
+    public static void printlnInfo() {
+        System.out.println("小惠惠");
+        System.out.println("萌妹子");
+        System.out.println("18岁");
+    }
+}
