4680b8eef744789aee47b5cfef199a780e9faf2c
ketyps <guanlin1600@gmail.com>
2026-06-27 08:26:01 +0800

    initialize

---
+++ basic-code/opp-code/src/com/itheima/test/Phone.java
@@ -0,0 +1,15 @@
+package com.itheima.test;
+
+public class Phone {
+    //属性(参数)
+    String brand;
+    double price;
+    //行为(函数)
+    public  void call(){
+        System.out.println("手机在打电话");
+    }
+
+    public void playgame(){
+        System.out.println("手机在玩游戏");
+    }
+}
