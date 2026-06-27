64bf9b9da96c25e2bbae8d159b416c4a6dae5ae7
ketyps <guanlin1600@gmail.com>
2026-06-27 09:18:29 +0800

    initialize

---
+++ basic-code/opp-code/src/com/itheima/test1/StringTest.java
@@ -0,0 +1,14 @@
+package com.itheima.test;
+
+public class StringTest {
+    public static void main(String[] args) {
+
+        String s = "wefipuhweiduhwiuehd";
+
+        int len = s.length();
+        System.out.println(len);
+
+        String ss = s.toUpperCase();
+        System.out.println(ss);
+    }
+}
