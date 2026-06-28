daf7fd70f1f9ad426d36eafe60f7697d68a07ff2
ketyp <guanlin1600@gmail.com>
2026-06-28 19:10:22 +0800

    none

---
+++ basic-code/oop-test/src/com/itheima/test6/PhoneTest.java
@@ -0,0 +1,32 @@
+package com.itheima.test6;
+
+public class PhoneTest {
+    static void main(String[] args) {
+        //1.创建一个数组
+        Phone[] arr = new Phone[3];
+
+        //2.创建手机对象
+        Phone p1 = new Phone("小米", 1999, "白色");
+        Phone p2 = new Phone("华为", 4999, "蓝色");
+        Phone p3 = new Phone("魅族", 3999, "红色");
+
+        //3.把手机对象添加到数组当中
+        arr[0] = p1;
+        arr[1] = p2;
+        arr[2] = p3;
+
+        //4.获取三部手机的平均价格
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            //i索引  arr[i] 元素 (手机对象)
+            Phone phone = arr[i];
+            sum += phone.getPrice();
+        }
+
+        //5.求平均值
+        //数据不能写死,尽量不写死
+        double avg2 = sum *1.0/ arr.length;
+
+        System.out.println(avg2);
+    }
+}
