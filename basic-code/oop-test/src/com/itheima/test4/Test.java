6ad05aeb8d6312cb4badefbc0050adc2b974f960
ketyps <guanlin1600@gmail.com>
2026-06-28 14:17:16 +0800

    initialize

---
+++ basic-code/oop-test/src/com/itheima/test4/Test.java
@@ -0,0 +1,32 @@
+package com.itheima.test4;
+
+import java.util.Scanner;
+
+public class Test {
+    static void main(String[] args) {
+        //键盘录入
+        //第一套体系:
+        //nextInt();接收整数
+        //nextDouble();接收小数
+        //next():接收字符串
+        //遇到空格,制表符,回车就停止接收.这些符号后面的数据就不会接收了
+
+        //第二套体系:
+        //nextline():接收字符串
+
+        /*Scanner sc =new Scanner(System.in);
+        System.out.println("请输入一个整数");
+        int num1=sc.nextInt();
+        System.out.println(num1);
+        System.out.println("请输入第二个整数");
+        int num2=sc.nextInt();
+        System.out.println(num2);*/
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入第一个字符串");
+        String line1=sc.nextLine();
+        System.out.println("请输入第二个字符串");
+        String line2=sc.nextLine();
+        System.out.println(line2);
+    }
+}
