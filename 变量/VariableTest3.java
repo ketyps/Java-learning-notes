7843282c46173a3873ed0b9fed93b63d39498b63
ketyps <171883081+ketyps@users.noreply.github.com>
2026-07-20 22:30:10 +0800

    调整day07与字符串变量示例代码

--- 变量/VariableTest3.java
+++ 变量/VariableTest3.java
@@ -1,13 +1,13 @@
 public class VariableTest3 {
     public static void main(String[] args) {
         //定义变量记录电影的名字
-        String name="送初恋回家";
+        String name = "送初恋回家";
         //定义变量记录主演的名字
-        String tostar="刘鑫 张雨提";
+        String tostar = "刘鑫 张雨提";
         //定义变量记录电影的年份
-        int year=2020;
+        int year = 2020;
         //定义变量记录电影的评分
-        double grade=9.0;
+        double grade = 9.0;
 
         //输出所有变量的值
         System.out.println(name);
