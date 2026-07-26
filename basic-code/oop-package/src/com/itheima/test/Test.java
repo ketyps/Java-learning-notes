8bd6da4e7dc055f30a9bf0f7f7bebd9b87724381
ketyps <guanlin1600@gmail.com>
2026-07-26 21:27:10 +0800

    initialize

---
+++ basic-code/oop-package/src/com/itheima/test/Test.java
@@ -0,0 +1,20 @@
+package com.itheima.test;
+
+
+public class Test {
+    public static void main(String[] args) {
+        // 创建对象
+    /*Student s = new Student();
+    s.setName("张三");
+    s.setAge(23);*/
+
+//        System.out.println(s.getName() + ", " + s.getAge());*/
+//    }
+//
+//    String s = "abc";
+//    System.out.println(s);
+
+        com.itheima.domain.Teacher t1 = new com.itheima.domain.Teacher();
+        com.itheima.domain2.Teacher t2 = new com.itheima.domain2.Teacher();
+    }
+}
