27e0eeae2e672b4e172798f3488af6a638c4f1da
ketyps <guanlin1600@gmail.com>
2026-07-28 22:14:06 +0800

    initialize

---
+++ basic-code/puzzlegame/src/com/itheima/ui/GameJFrame.java
@@ -0,0 +1,10 @@
+package com.itheima.ui;
+
+import javax.swing.*;
+
+public class GameJFrame extends JFrame {
+    public GameJFrame(){
+        this.setSize(603,680);
+        this.setVisible(true);
+    }
+}
