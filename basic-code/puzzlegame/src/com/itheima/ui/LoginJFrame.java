b890a1ef3764e3b6324a609546897aa554b12f29
ketyps <171883081+ketyps@users.noreply.github.com>
2026-07-28 22:14:06 +0800

    新增拼图游戏登录注册界面框架

---
+++ basic-code/puzzlegame/src/com/itheima/ui/LoginJFrame.java
@@ -0,0 +1,14 @@
+package com.itheima.ui;
+
+import javax.swing.*;
+
+public class LoginJFrame extends JFrame {
+    //登录界面
+    public LoginJFrame(){
+        //在创建登录界面的时候,同时给这个界面去设置一些信息
+        //比如,宽高,直接展示出来
+
+        this.setSize(488,430);
+        this.setVisible(true);
+    }
+}
---
+++ basic-code/puzzlegame/src/com/itheima/ui/RegisterJFrame.java
@@ -0,0 +1,11 @@
+package com.itheima.ui;
+
+import javax.swing.*;
+
+public class RegisterJFrame extends JFrame {
+    //注册界面
+    public RegisterJFrame(){
+        this.setSize(480,500);
+        this.setVisible(true);
+    }
+}
