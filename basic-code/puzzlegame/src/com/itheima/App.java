5d028cc93ef90ff0438198e57a5bc83fdd690515
ketyps <guanlin1600@gmail.com>
2026-07-31 23:30:17 +0800

    initialize

---
+++ basic-code/puzzlegame/src/com/itheima/App.java
@@ -0,0 +1,11 @@
+package com.itheima;
+
+import com.itheima.ui.GameJFrame;
+
+// 程序的入口类
+// 运行本类中的 main 方法即可启动拼图游戏
+public class App {
+    public static void main(String[] args) {
+        new GameJFrame();
+    }
+}
---
+++ basic-code/puzzlegame/src/com/itheima/test/MyJFrame3.java
@@ -0,0 +1,51 @@
+package com.itheima.test;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class MyJFrame3 extends JFrame implements KeyListener {
+
+    public MyJFrame3() {
+        //设置界面的宽高
+        this.setSize(603, 680);
+        //设置界面的标题
+        this.setTitle("拼图单机版 v1.0");
+        //设置界面置顶
+        this.setAlwaysOnTop(true);
+        //设置界面居中
+        this.setLocationRelativeTo(null);
+        //设置关闭模式
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        //取消默认的居中放置，只有取消了才会按照XY轴的形式添加组件
+        this.setLayout(null);
+
+        //给整个窗体添加键盘监听
+        this.addKeyListener(this);
+
+
+        //让整个界面显示出来
+        this.setVisible(true);
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        System.out.println("按下不松");
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        System.out.println("松开按键");
+        //获取键盘上每一个变量的编号
+        //每一个按键都有一个编号与之对应
+        int code = e.getKeyCode();
+        System.out.println(code);
+    }
+}
